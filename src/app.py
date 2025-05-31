2aee2b923928ead5332c904ba3f30cd0d8e81f4f
Davide Bianco <davidebianco1@icloud.com>
2025-05-31 15:14:30 +0200

    update app.py to launch server at 0.0.0.0

--- src/app.py
+++ src/app.py
@@ -22,4 +22,4 @@ app.register_blueprint(public_bp)
 app.register_blueprint(private_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
