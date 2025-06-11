c7a7ac79bfab352ee586f464c7fb4d28fc1b5bfe
Davide Bianco <davidebianco1@icloud.com>
2025-06-11 13:53:27 +0200

    fix login signup templates and routes

--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -18,7 +18,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash("Logout effettuato con successo!", "success")
+    flash("Logout effettuato con successo!", "info")
     return redirect(url_for("public.home"))
 
 # ---------- form actions ------------------------
@@ -26,22 +26,27 @@ def logout():
 @auth_bp.route("/subscribe", methods=["POST"])
 def subscribe():
     nome = request.form.get("nome")
+    if not nome:
+        flash("Inserisci un nome valido", "danger")
+        return redirect(url_for("auth.signup"))
     cognome = request.form.get("cognome")
+    if not cognome:
+        flash("Inserisci un cognome valido", "danger")
     email = request.form.get("email")
-    if "@" not in email:
-        flash("Email non valida", "error")
+    if not email or "@" not in email:
+        flash("Email non valida", "danger")
         return redirect(url_for("auth.signup"))
     if utenti_dao.get_utente_by_email(email):
-        flash("Email già utilizzata", "error")
+        flash("Email già utilizzata", "danger")
         return redirect(url_for("auth.signup"))
     password = request.form.get("password")
     if len(password) < 8:
-        flash("Password troppo corta", "error")
+        flash("La password deve contenere almeno 8 caratteri", "danger")
         return redirect(url_for("auth.signup"))
     password_hash = generate_password_hash(password, method="scrypt")
     ruolo = request.form.get("ruolo")
     if ruolo not in ("organizzatore", "partecipante"):
-        flash("Ruolo non valido", "error")
+        flash("Ruolo non valido", "danger")
         return redirect(url_for("auth.signup"))
     id = utenti_dao.add_utente(nome, cognome, email, password_hash, ruolo)
     user = User(
@@ -59,10 +64,16 @@ def subscribe():
 @auth_bp.route("/authenticate", methods=["POST"])
 def authenticate():
     email = request.form.get("email")
+    if not email or "@" not in email:
+        flash("Email non valida", "danger")
+        return redirect(url_for("auth.login")) 
     password = request.form.get("password")
+    if not password:
+        flash("Inserisci la password", "danger")
+        return redirect(url_for("auth.login"))
     utente = utenti_dao.get_utente_by_email(email)
     if not utente or not check_password_hash(utente["password"], password):
-        flash("Credenziali non valide", "error")
+        flash("Credenziali non valide", "danger")
         return redirect(url_for("auth.login"))
     user = User(
         id=utente["id"],
