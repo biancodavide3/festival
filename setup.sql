5b8f14670c6b1dfc9180bf10e37490ea9ecedca1
Davide Bianco <davidebianco1@icloud.com>
2025-06-11 18:15:10 +0200

    recompile database for more appropriate descriptions

--- setup.sql
+++ setup.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS biglietti_giorni (
     FOREIGN KEY(id_biglietto) REFERENCES biglietti(id)
 );
 
--- Dati (la password per ogni utente e' "password")
+-- dati (la password per ogni utente e' "password")
 
 INSERT INTO utenti (nome, cognome, email, password, ruolo) VALUES
 ('Andrea', 'Bianchi', 'andrea@gmail.com', 'scrypt:32768:8:1$JfuxD9WFwpX33phC$53a19f7b55a866cb760c4fe6dd58752c21f236182cc4118d61bd8b0abd563d76b10c792dcda0f1a700a5a13bec620ccf4b0e959f863f89fcdd503d0a8001be9d', 'organizzatore'),
@@ -53,25 +53,25 @@ INSERT INTO utenti (nome, cognome, email, password, ruolo) VALUES
 INSERT INTO performances (nome_artista, giorno, orario, durata, descrizione, palco, genere, immagine, pubblicato, id_organizzatore)
 VALUES
 -- Venerdì
-('Solar Echoes', 'Venerdi', '18:00', 60, 'Un viaggio sonoro attraverso ambient e downtempo elettronico.', 'Palco A', 'Elettronica', '/static/images/uploaded/4a1e9b25-9f3c-4ea5-a7b2-b2d68f453b91.jpg', 1, 1),
-('The Wood Spirits', 'Venerdi', '19:15', 45, 'Folk acustico con strumenti tradizionali da tutto il mondo.', 'Palco B', 'World Music', '/static/images/uploaded/fd1a3e92-6c90-44e2-88b6-080b09e66c98.jpg', 1, 1),
-('Nocturna Loop', 'Venerdi', '20:30', 50, 'Live set techno dark su visuali oniriche.', 'Palco C', 'Techno', '/static/images/uploaded/0f2b2e93-5c18-4a2e-8e61-0150982c12c4.jpg', 1, 2),
-('Mystic Flows', 'Venerdi', '21:45', 40, 'Jazz psichedelico in ambientazione multisensoriale.', 'Palco A', 'Jazz', '/static/images/uploaded/2e4c8cb1-0e0e-4b9a-9c6f-3a22f3c64f2d.jpg', 1, 2),
-('Hidden Frequencies', 'Venerdi', '23:00', 60, 'Performance sperimentale binaurale.', 'Palco B', 'Sperimentale', '/static/images/uploaded/a92f27d3-25cb-41f3-9e75-85e17a14ac08.jpg', 0, 1),
+('Solar Echoes', 'Venerdi', '18:00', 60, 'Energia pura e riff potenti in una performance rock coinvolgente.', 'Palco A', 'Rock', '/static/images/uploaded/4a1e9b25-9f3c-4ea5-a7b2-b2d68f453b91.jpg', 1, 1),
+('The Wood Spirits', 'Venerdi', '19:15', 45, 'Rock energico con melodie accattivanti e testi appassionanti.', 'Palco B', 'Rock', '/static/images/uploaded/fd1a3e92-6c90-44e2-88b6-080b09e66c98.jpg', 1, 1),
+('Nocturna Loop', 'Venerdi', '20:30', 50, 'Pop moderno con influenze dance e arrangiamenti freschi.', 'Palco C', 'Pop', '/static/images/uploaded/0f2b2e93-5c18-4a2e-8e61-0150982c12c4.jpg', 1, 2),
+('Mystic Flows', 'Venerdi', '21:45', 40, 'Il massimo della musica elettronica.', 'Palco A', 'Electronic', '/static/images/uploaded/2e4c8cb1-0e0e-4b9a-9c6f-3a22f3c64f2d.jpg', 1, 2),
+('Hidden Frequencies', 'Venerdi', '23:00', 60, 'Rock alternativo con un sound fresco e originale.', 'Palco B', 'Rock', '/static/images/uploaded/a92f27d3-25cb-41f3-9e75-85e17a14ac08.jpg', 0, 1),
 
 -- Sabato
-('Echo Nomads', 'Sabato', '17:00', 60, 'Fusion tra sonorità mediorientali e synth analogici.', 'Palco A', 'Fusion', '/static/images/uploaded/719f4c01-b394-40b1-b1ef-31d0e8cb29f9.jpg', 1, 1),
-('Lumen Sky', 'Sabato', '18:30', 45, 'Indie-pop con elementi elettronici ambient.', 'Palco B', 'Indie Pop', '/static/images/uploaded/6b4c1a5a-2bb9-4f1e-a2c2-cf39cb8c46cd.jpg', 1, 1),
-('Drift Mechanics', 'Sabato', '19:45', 60, 'Drum & Bass atmosferico con video generativi.', 'Palco C', 'D&B', '/static/images/uploaded/89134b82-41e7-4552-bfaa-fb687261f4ee.jpg', 1, 2),
-('Aurora Collective', 'Sabato', '21:00', 50, 'Ensemble elettronico + strumenti dal vivo.', 'Palco A', 'Live Set', '/static/images/uploaded/dfb1d95d-d590-4e0c-a8ec-0a631fbcc89c.jpg', 1, 2),
-('Nebula Bloom', 'Sabato', '22:30', 40, 'Trio vocale femminile in performance visiva.', 'Palco B', 'Vocal', '/static/images/uploaded/3e80cf79-1c64-4045-9d99-fbe05843f9cd.jpg', 0, 1),
+('Echo Nomads', 'Sabato', '17:00', 60, 'Brani potenti e coinvolgenti che fanno battere il cuore.', 'Palco A', 'Rock', '/static/images/uploaded/719f4c01-b394-40b1-b1ef-31d0e8cb29f9.jpg', 1, 1),
+('Lumen Sky', 'Sabato', '18:30', 45, 'Indie-pop con elementi elettronici ambient.', 'Palco B', 'Pop', '/static/images/uploaded/6b4c1a5a-2bb9-4f1e-a2c2-cf39cb8c46cd.jpg', 1, 1),
+('Drift Mechanics', 'Sabato', '19:45', 60, 'Armonia degli strumenti allo stato puro', 'Palco C', 'Classica', '/static/images/uploaded/89134b82-41e7-4552-bfaa-fb687261f4ee.jpg', 1, 2),
+('Aurora Collective', 'Sabato', '21:00', 50, 'Una scarica di energia con chitarre distorte e ritmi serrati.', 'Palco A', 'Rock', '/static/images/uploaded/dfb1d95d-d590-4e0c-a8ec-0a631fbcc89c.jpg', 1, 2),
+('Nebula Bloom', 'Sabato', '22:30', 40, 'Trio vocale femminile in performance visiva.', 'Palco B', 'Pop', '/static/images/uploaded/3e80cf79-1c64-4045-9d99-fbe05843f9cd.jpg', 0, 1),
 
 -- Domenica
-('Raindrop Ritual', 'Domenica', '16:00', 45, 'Percussioni tribali e canto armonico.', 'Palco A', 'Tribal', '/static/images/uploaded/efc5ea0d-195f-4e35-bb76-2c119d1c60e1.jpg', 1, 1),
-('Kaleido Waves', 'Domenica', '17:15', 50, 'Pop psichedelico in quadrofonia immersiva.', 'Palco B', 'Psy Pop', '/static/images/uploaded/cc2c4b6f-90f7-45d0-81e0-d650e1901e47.jpg', 1, 1),
-('Analog Forest', 'Domenica', '18:30', 60, 'Composizioni ambient ispirate alla natura.', 'Palco C', 'Ambient', '/static/images/uploaded/90a924f9-3f7d-4cf0-a01c-c7e384bf99cd.jpg', 1, 2),
-('Digital Sorcery', 'Domenica', '20:00', 55, 'Set IDM con visuals ispirati al glitch art.', 'Palco A', 'IDM', '/static/images/uploaded/ce2b2a06-2cbf-4f3b-b07d-f73a5bfe30d4.jpg', 1, 2),
-('The Final Echo', 'Domenica', '21:30', 60, 'Set collettivo con chiusura multisensoriale.', 'Palco B', 'Multigenere', '/static/images/uploaded/ae16a19b-fd8d-4d13-b1f2-9a80e42e02a1.jpg', 0, 1);
+('Raindrop Ritual', 'Domenica', '16:00', 45, 'Una band capace di far vibrare ogni palco con il loro rock autentico.', 'Palco A', 'Rock', '/static/images/uploaded/efc5ea0d-195f-4e35-bb76-2c119d1c60e1.jpg', 1, 1),
+('Kaleido Waves', 'Domenica', '17:15', 50, 'Rock psichedelico da brividi.', 'Palco B', 'Rock', '/static/images/uploaded/cc2c4b6f-90f7-45d0-81e0-d650e1901e47.jpg', 1, 1),
+('Analog Forest', 'Domenica', '18:30', 60, 'Composizione Rock ispirata alla natura.', 'Palco C', 'Rock', '/static/images/uploaded/90a924f9-3f7d-4cf0-a01c-c7e384bf99cd.jpg', 1, 2),
+('Digital Sorcery', 'Domenica', '20:00', 55, 'Pop fresco che unisce ballate emotive e brani uptempo.', 'Palco A', 'Pop', '/static/images/uploaded/ce2b2a06-2cbf-4f3b-b07d-f73a5bfe30d4.jpg', 1, 2),
+('The Final Echo', 'Domenica', '21:30', 60, 'Sound pulito e produzioni curate per un pop irresistibile.', 'Palco B', 'Pop', '/static/images/uploaded/ae16a19b-fd8d-4d13-b1f2-9a80e42e02a1.jpg', 0, 1);
 
 
 INSERT INTO biglietti (id_partecipante, tipo) VALUES
